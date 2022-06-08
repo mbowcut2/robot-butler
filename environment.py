b1aa3487fe9335fd98090e1e1dedb005fad80f98
McKay Bowcut <mbowcut@abnormalsecurity.com>
2022-06-08 12:58:11 -0600

    added env and robot classes

---
+++ environment.py
@@ -0,0 +1,9 @@
+class Environment:
+	"""
+	This is the text environment that interacts with the user and the robot.
+
+	It should initialize the robot and act as a sort of narrator/DM. If the the goal is to simulate an agent in a physical environment, then we may wish to populate this text environment with objects that the robot can perceive and interact with.
+
+
+	"""
+	pass
---
+++ robot.py
@@ -0,0 +1,6 @@
+class Robot:
+	"""
+	This is the agent. It will receive NL instructions from the user and then perform actions in the environment.
+	"""
+
+	pass
