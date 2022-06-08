054943f7c8ac022949dde9d639570b9945c3f090
McKay Bowcut <mbowcut@abnormalsecurity.com>
2022-06-08 12:31:27 -0600

    added grammar.py file

---
+++ grammar.py
@@ -0,0 +1,9 @@
+"""
+This file contains lists of words used to populate templates
+"""
+
+ROOMS = ['living room', 'bathroom', 'bedroom', 'kitchen']
+OBJECTS = ['ball', 'toy car', 'chair', 'table', 'toilet', 'lamp', 'couch']
+UNUSUAL_OBJECTS = ['dead body', 'sword', 'the one ring']
+AMBIGUOUS_OBJECTS = ['helicopter', 'airplane', 'train'] # These objects would be inferred to be toys in context. I'm curious to see how the model would handle them.
+
